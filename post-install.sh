59235b7c6d7bcb01b202b3090abd120d36a8337a
Mr-Yellow <mr-yellow@mr-yellow.com>
2016-08-21 09:57:19 +1000

    Install and build ROS

--- post-install.sh
+++ post-install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 # As root
 # adduser yellow sudo
--- ros-kinetic.sh
+++ ros-kinetic.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 if [ ! -f /etc/apt/sources.list.d/ros-latest.list ]
 then
@@ -16,18 +17,31 @@ if [ ! -f /etc/ros/rosdep/sources.list.d/20-default.list ]
 then
   echo "Initialising rosdep."
   sudo rosdep init
-  rosdep update
 fi
+echo "Updating rosdep."
+rosdep update
 
-echo "Creating catkin workspace."
-mkdir -p ~/work/ros/catkin_ws
+if [ ! -d ~/work/ros/catkin_ws ]
+then
+  echo "Creating Catkin workspace."
+  mkdir -p ~/work/ros/catkin_ws
+fi
 cd ~/work/ros/catkin_ws
 
 if [ ! -f ~/work/ros/catkin_ws/kinetic-desktop-full-wet.rosinstall ]
 then
   echo "Generating ROS workspace."
   rosinstall_generator desktop_full --rosdistro kinetic --deps --wet-only > kinetic-desktop-full-wet.rosinstall
-  wstool init -j4 src kinetic-desktop-full-wet.rosinstall
+  wstool init -j8 src kinetic-desktop-full-wet.rosinstall
+else
+  echo "Updating ROS workspace."
+  wstool update -j8 -t src
 fi
-echo "Installing ROS packages."
-rosdep install --from-paths src --ignore-src --rosdistro kinetic -y --os debian:jessie
+
+echo "Installing dependency packages."
+rosdep install --from-paths src --ignore-src --rosdistro kinetic -y --os $(lsb_release -si | awk '{print tolower($0)}'):$(lsb_release -sc)
+
+echo "Disabling CUDA support."
+catkin config --cmake-args -DWITH_CUDA=OFF -DBUILD_opencv_gpu=OFF
+echo "Building ROS workspace."
+catkin build
