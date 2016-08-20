eaef1d029bfb9f8f2b3efe448c60f5f136000ddd
Mr-Yellow <mr-yellow@mr-yellow.com>
2016-08-21 08:20:38 +1000

    Initial commit

---
+++ post-install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# As root
+# adduser yellow sudo
+
+mkdir -p ~/src/
+wget -O ~/src/atom-amd64.deb https://github.com/atom/atom/releases/download/v1.9.9/atom-amd64.deb
+wget -O ~/src/google-chrome-stable_current_amd64.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+wget -O ~/src/steam_latest.deb https://steamcdn-a.akamaihd.net/client/installer/steam.deb
+
+dpkg -i ~/src/atom-amd64.deb
+dpkg -i ~/src/google-chrome-stable_current_amd64.deb
+dpkg -i ~/src/steam_latest.deb
+
+# Include contrib and non-free for nvidia drivers.
+sudo sed --in-place=.old -e 's/main$/main contrib non-free/g' /etc/apt/sources.list
+sudo apt-get update
+
+# Bumblebee Hybrid graphics
+sudo apt-get install -y bumblebee-nvidia primus mesa-utils
+sudo adduser $USER bumblebee
+#optirun glxgears -info
+
+# Redshift blue-light management
+sudo apt-get install -y redshift-plasmoid
