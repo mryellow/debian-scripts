7f3f3ee1d0bac5acbe4211c1bb2d74d76eeab2ff
Mr-Yellow <mr-yellow@mr-yellow.com>
2016-08-21 12:44:55 +1000

    Use custom rosinstall

--- ros-kinetic.sh
+++ ros-kinetic.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 set -e
 
-if [ ! -f /etc/apt/sources.list.d/ros-latest.list ]
+CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+KINETIC_DIR="~/work/ros-kinetic/catkin_ws"
+
+echo "CURRENT_DIR: $CURRENT_DIR"
+echo "KINETIC_DIR: $KINETIC_DIR"
+
+if [ ! -f /etc/apt/sources.list.d/ros-latest.list ];
 then
   echo "Configure apt sources."
   sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
@@ -14,7 +20,7 @@ sudo apt-get install -y python-rosdep python-rosinstall-generator python-wstool 
 # FIXME: `python-numpy` not included in OpenCV3 dependencies.
 sudo apt-get install -y python-catkin-tools python-numpy
 
-if [ ! -f /etc/ros/rosdep/sources.list.d/20-default.list ]
+if [ ! -f /etc/ros/rosdep/sources.list.d/20-default.list ];
 then
   echo "Initialising rosdep."
   sudo rosdep init
@@ -22,18 +28,24 @@ fi
 echo "Updating rosdep."
 rosdep update
 
-if [ ! -d ~/work/ros/catkin_ws ]
+if [ ! -f "$KINETIC_DIR" ];
 then
   echo "Creating Catkin workspace."
-  mkdir -p ~/work/ros/catkin_ws
+  mkdir -p $KINETIC_DIR
+  echo "$CURRENT_DIR/kinetic-desktop-custom.rosinstall $KINETIC_DIR/kinetic-desktop-custom.rosinstall"
+  cp $CURRENT_DIR/kinetic-desktop-custom.rosinstall $KINETIC_DIR/kinetic-desktop-custom.rosinstall
 fi
-cd ~/work/ros/catkin_ws
+echo "Changing directory to $KINETIC_DIR"
+cd $KINETIC_DIR
 
-if [ ! -f ~/work/ros/catkin_ws/kinetic-desktop-full-wet.rosinstall ]
+#if [ ! -f $KINETIC_DIR/kinetic-desktop-full-wet.rosinstall ];
+if [ ! -f "$KINETIC_DIR/src" ];
 then
   echo "Generating ROS workspace."
-  rosinstall_generator desktop_full --rosdistro kinetic --deps --wet-only > kinetic-desktop-full-wet.rosinstall
-  wstool init -j8 src kinetic-desktop-full-wet.rosinstall
+  # FIXME: Using own `.rosinstall` file to avoid broken packages. Could merge my changes.
+  #rosinstall_generator desktop_full --rosdistro kinetic --deps --wet-only > kinetic-desktop-full-wet.rosinstall
+  #wstool init -j8 src kinetic-desktop-full-wet.rosinstall
+  wstool init -j8 src kinetic-desktop-custom.rosinstall
 else
   echo "Updating ROS workspace."
   wstool update -j8 -t src
@@ -47,7 +59,7 @@ catkin config --cmake-args -DWITH_CUDA=OFF -DBUILD_opencv_gpu=OFF
 
 echo "Build workspace? [Y/n]"
 read input_variable
-if echo "$input_variable" | grep -iq "^n" ;
+if echo "$input_variable" | grep -iq "^n";
 then
   echo "Skipping workspace build."
 else
