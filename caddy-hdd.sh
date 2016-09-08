c01437e6f9c3162feea071ea1b7cc045289deb27
Mr-Yellow <mr-yellow@mr-yellow.com>
2016-09-08 14:55:38 +1000

    Create in user path

--- caddy-hdd.sh
+++ caddy-hdd.sh
@@ -2,5 +2,10 @@
 
 if ! grep "e3b67f0e-608b-4c8e-8cd7-f962e64cd6bf" /etc/apt/sources.list;
 then
-  echo "UUID=e3b67f0e-608b-4c8e-8cd7-f962e64cd6bf /home/$USER/caddy ext4 errors=remount-ro 0 1" >> /etc/fstab
+  DIR="/home/$USER/caddy"
+  mkdir -p $DIR
+  echo "UUID=e3b67f0e-608b-4c8e-8cd7-f962e64cd6bf $DIR ext4 errors=remount-ro 0 1" | sudo tee -a /etc/fstab
+  sudo mount $DIR
+
+  # TODO: Should comment out /media/cdrom0
 fi
