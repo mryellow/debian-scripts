acb86b7add72ad5f70e2ade5da2381a4fdf1772c
Mr-Yellow <mr-yellow@mr-yellow.com>
2016-11-17 16:55:31 +1030

    Configure USB swap for odroid

---
+++ post-install-odroid.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -o errexit
+set -o pipefail
+
+RED='\033[0;31m'
+YEL='\033[1;33m'
+NC='\033[0m' # No Color
+
+if ! grep swap /etc/fstab;
+then
+  echo -e "${YEL}Configure USB swap.${NC}"
+  sudo sh -c 'echo "UUID=68e068ff-f919-4620-9a91-953dc1137fcb none swap sw,pri=5 0 0" >> /etc/fstab'
+  sudo swapon -a
+fi
+
