2067d244ec83ee33676c1bb3d32e19aba784cb26
Mr-Yellow <mr-yellow@mr-yellow.com>
2017-07-12 19:12:49 +1000

    Backup to external

--- backup.sh
+++ backup.sh
@@ -3,4 +3,5 @@
 rsync -aP ~/caddy/Pictures/ ~/Pictures
 #rsync -aP ~/caddy/Pictures/Canon/converted/ ~/Dropbox/Pictures/Canon/converted
 rsync -aP ~/caddy/Pictures/ /media/$USER/9cddbbd5-d369-4959-9aa2-240df4697642/Pictures
+rsync -aP ~/caddy/Pictures/ /media/$USER/EXT/Pictures
 
