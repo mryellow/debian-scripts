2a5d37ceade32ca1792a18967582ad1aca1d7ba9
Mr-Yellow <mr-yellow@mr-yellow.com>
2019-01-29 18:13:48 +1100

    ICC and HDD

--- backup.sh
+++ backup.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-rsync -aP ~/caddy/Pictures/ ~/Pictures
+#rsync -aP ~/caddy/Pictures/ ~/Pictures
 #rsync -aP ~/caddy/Pictures/Canon/converted/ ~/Dropbox/Pictures/Canon/converted
-rsync -aP ~/caddy/Pictures/ /media/$USER/9cddbbd5-d369-4959-9aa2-240df4697642/Pictures
-rsync -aP ~/caddy/Pictures/ /media/$USER/EXT/Pictures
+#rsync -aP ~/caddy/Pictures/ /media/$USER/9cddbbd5-d369-4959-9aa2-240df4697642/Pictures
+#rsync -aP ~/caddy/Pictures/ /media/$USER/EXT/Pictures
+
+rsync -aP ~/Pictures/Canon/ /media/user/e3b67f0e-608b-4c8e-8cd7-f962e64cd6bf/Pictures/Canon
+
 
--- load-icc.sh
+++ load-icc.sh
@@ -2,4 +2,6 @@
 
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-xcalib -d :0 "${CURRENT_DIR}/assets/icc/N156BGE-L41 #1 2016-11-24 04-15 2.2 M-S XYZLUT+MTX.icc"
+#xcalib -d :0 "${CURRENT_DIR}/assets/icc/N156BGE-L41 #1 2016-11-24 04-15 2.2 M-S XYZLUT+MTX.icc"
+xcalib -d :0 "${CURRENT_DIR}/assets/icc/Screen 1 #1 2019-01-01 18-08 2.2 M-S XYZLUT+MTX.icc"
+
